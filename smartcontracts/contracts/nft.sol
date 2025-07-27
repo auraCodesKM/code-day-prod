2ddb80add31ef2fac51af73f2ae1b1ae0a153437
ReservedSnow <sandhu.tanvirsingh@yahoo.com>
2025-07-27 10:39:12 +0530

    Update nft.sol

--- smartcontracts/contracts/nft.sol
+++ smartcontracts/contracts/nft.sol
@@ -3,71 +3,24 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract SpaceNFT is ERC721URIStorage, Ownable, ReentrancyGuard {
     uint256 public tokenCounter;
     uint256 public mintFee = 0.01 ether;
     address public treasury;
-    
-    // Ship type fees
-    uint256 public ship1Fee = 0.01 ether;
-    uint256 public ship2Fee = 0.025 ether;
-    uint256 public ship3Fee = 0.05 ether;
+    address public gameContract;
 
-    constructor(address _treasury) ERC721("SpaceShip", "SHIP") {
+    constructor(address _treasury)
+        ERC721("SpaceShip", "SHIP")
+        Ownable(msg.sender)
+    {
         require(_treasury != address(0), "Invalid treasury address");
         tokenCounter = 1;
         treasury = _treasury;
     }
 
-    function mintShip1(string memory tokenURI) external payable nonReentrant returns (uint256) {
-        require(msg.value == ship1Fee, "Incorrect ETH amount for Ship 1");
-
-        // Transfer mint fee to treasury
-        (bool sent, ) = treasury.call{value: msg.value}("");
-        require(sent, "ETH transfer failed");
-
-        uint256 tokenId = tokenCounter;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        tokenCounter++;
-
-        return tokenId;
-    }
-
-    function mintShip2(string memory tokenURI) external payable nonReentrant returns (uint256) {
-        require(msg.value == ship2Fee, "Incorrect ETH amount for Ship 2");
-
-        // Transfer mint fee to treasury
-        (bool sent, ) = treasury.call{value: msg.value}("");
-        require(sent, "ETH transfer failed");
-
-        uint256 tokenId = tokenCounter;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        tokenCounter++;
-
-        return tokenId;
-    }
-
-    function mintShip3(string memory tokenURI) external payable nonReentrant returns (uint256) {
-        require(msg.value == ship3Fee, "Incorrect ETH amount for Ship 3");
-
-        // Transfer mint fee to treasury
-        (bool sent, ) = treasury.call{value: msg.value}("");
-        require(sent, "ETH transfer failed");
-
-        uint256 tokenId = tokenCounter;
-        _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        tokenCounter++;
-
-        return tokenId;
-    }
-
-    // Keep the original mintShip function for backward compatibility
-    function mintShip(string memory tokenURI) external payable nonReentrant returns (uint256) {
+    function mintShip1() external payable nonReentrant returns (uint256) {
         require(msg.value == mintFee, "Incorrect ETH amount");
 
         // Transfer mint fee to treasury
@@ -76,14 +29,62 @@ contract SpaceNFT is ERC721URIStorage, Ownable, ReentrancyGuard {
 
         uint256 tokenId = tokenCounter;
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(
+            tokenId,
+            "ipfs://bafybeibmk3erwtfjzdfceaoijqn3jpiqg3hg4lz7nvnlg4pcakpu5hmr54/1.json"
+        );
         tokenCounter++;
 
         return tokenId;
     }
 
-    function burn(uint256 tokenId) external {
-        require(ownerOf(tokenId) == msg.sender, "Not your ship");
+    function mintShip2() external payable nonReentrant returns (uint256) {
+        require(msg.value == 0.025 ether, "Incorrect ETH amount");
+
+        // Transfer mint fee to treasury
+        (bool sent, ) = treasury.call{value: msg.value}("");
+        require(sent, "ETH transfer failed");
+
+        uint256 tokenId = tokenCounter;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            "ipfs://bafybeibmk3erwtfjzdfceaoijqn3jpiqg3hg4lz7nvnlg4pcakpu5hmr54/2.json"
+        );
+        tokenCounter++;
+
+        return tokenId;
+    }
+
+    function mintShip3() external payable nonReentrant returns (uint256) {
+        require(msg.value == 0.05 ether, "Incorrect ETH amount");
+
+        // Transfer mint fee to treasury
+        (bool sent, ) = treasury.call{value: msg.value}("");
+        require(sent, "ETH transfer failed");
+
+        uint256 tokenId = tokenCounter;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            "ipfs://bafybeibmk3erwtfjzdfceaoijqn3jpiqg3hg4lz7nvnlg4pcakpu5hmr54/3.json"
+        );
+        tokenCounter++;
+
+        return tokenId;
+    }
+
+    modifier onlyGame() {
+        require(msg.sender == gameContract, "Not authorized");
+        _;
+    }
+
+    function setGameContract(address _game) external onlyOwner {
+        require(_game != address(0), "Invalid address");
+        gameContract = _game;
+    }
+
+    function burn(uint256 tokenId) external onlyGame {
         _burn(tokenId);
     }
 
